8af5328984be485b330df40eacc42a259cdb5a84
Chris Manzi <chrisdmanzi@gmail.com>
2019-01-16 23:04:13 +0000

    Automatically backed up by Learn

--- index.js
+++
@@ -1,7 +0,0 @@
-// adfunction theBeatlesPlay(players, instruments) {
-    var allPlayers = [];
-    for (var i=0; i < players.length; i++) {
-        allPlayers.push(players[i] + " plays " + instruments[i]);
-    }
-    return allPlayers;
-}d solution here
